e0481b16c710a7fcb96b29d211b8cfa89c1f7c12
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-02-17 14:57:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from flask import Flask, jsonify
-import os
+from io import BytesIO
+from flask import Flask, send_file, make_response, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def view_cv():
+    cv_path = 'cv.pdf'
+    with open(cv_path, 'rb') as cv_file:
+        # Leer el archivo PDF en memoria
+        cv_content = cv_file.read()
+        # Crear un objeto BytesIO
+        cv_stream = BytesIO(cv_content)
+        # Crear una respuesta con el archivo PDF
+        response = make_response(cv_stream.getvalue())
+        # Establecer el tipo de contenido a 'application/pdf'
+        response.headers.set('Content-Type', 'application/pdf')
+        # Establecer el encabezado para mostrar el PDF en el navegador
+        response.headers.set('Content-Disposition', 'inline', filename='cv.pdf')
+        return response
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
